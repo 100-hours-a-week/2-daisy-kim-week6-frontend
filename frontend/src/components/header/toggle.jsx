bdc704831b951c34586c62a2c4ca49b2a852f7ad
okiidokim <127471879+okiidokim@users.noreply.github.com>
2025-03-18 04:16:23 +0900

    [feat] 로그아웃, 탈퇴
    - 탈퇴
    - 로그아웃 구현 완료
    - 게시글 삭제 남음
    - 팝업 구현 전부 남음
    - 아직 리팩토링 필요

--- frontend/src/components/header/toggle.jsx
+++ frontend/src/components/header/toggle.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import * as S from './toggleStyle';
+import api from '../../utils/axios';
 
 export default function Toggle() {
   const nav = useNavigate();
+  async function logout() {
+    try {
+      await api.post('/user/logout');
+    } catch (e) {
+      console.log(e.response);
+    } finally {
+      nav('/');
+    }
+  }
   return (
     <S.Wrapper>
       <S.Button onClick={() => nav('/user/edit')}>회원 정보 수정</S.Button>
       <S.Button onClick={() => nav('/user/edit/password')}>
         비밀번호 수정
       </S.Button>
-      <S.Button onClick={() => nav('/')}>로그아웃</S.Button>
+      <S.Button onClick={logout}>로그아웃</S.Button>
     </S.Wrapper>
   );
 }
