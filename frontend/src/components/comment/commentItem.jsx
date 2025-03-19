dff9b3c0c0ac65aae2109bba405ee4df45eaf748
okiidokim <127471879+okiidokim@users.noreply.github.com>
2025-03-19 17:55:52 +0900

    [feat] 팝업 구현 완료
    - 게시글 삭제
    - 댓글 삭제
    - 회원 탈퇴

--- frontend/src/components/comment/commentItem.jsx
+++ frontend/src/components/comment/commentItem.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './style/commentItemStyle';
 import EditDelete from '../button/EditDelete';
 import UserInfo from '../board/userInfo';
+import Popup from '../popup/Popup';
 
 export default function CommentItem({ item, editFunction, deleteFunction }) {
+  const [show, setShow] = useState(false);
+
   return (
     <S.CommentItem key={item.id}>
       <S.CommentLeft>
@@ -13,7 +16,15 @@ export default function CommentItem({ item, editFunction, deleteFunction }) {
       {item.myComment ? (
         <EditDelete
           editFunction={editFunction}
-          deleteFunction={deleteFunction}
+          deleteFunction={() => setShow(true)}
+        />
+      ) : null}
+      {show ? (
+        <Popup
+          title="댓글을 삭제하시겠습니까?"
+          content="삭제한 내용은 복구 할 수 없습니다."
+          cancelFunction={() => setShow(false)}
+          okFunction={deleteFunction}
         />
       ) : null}
     </S.CommentItem>
